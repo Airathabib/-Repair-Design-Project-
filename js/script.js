5d32f20c5f520dd1b206f71b58c262738d7a849b
Airat <airat.24@mail.ru>
2023-11-02 18:52:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+const formBtn = document.querySelector('.form__btn');
+formBtn.addEventListener('click', (e) => {
+  e.preventDefault()
+});
+
+const headerPhone = document.querySelector('.link__phone');
+headerPhone.addEventListener('click', (e) => {
+  e.preventDefault()
+});
+
+const footerPhone = document.querySelector('.footer__phone');
+footerPhone.addEventListener('click', (e) => {
+  e.preventDefault()
+});
+
+
+
